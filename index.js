39fb04187ff8cc260d96e21cd3b5b58ef85417f1
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-02-21 20:30:48 +0600

    navigation done for mobile and pc

---
+++ index.js
@@ -0,0 +1,35 @@
+const featuresList = document.querySelector(".featuresList");
+const list1 = document.querySelector(".list1");
+const companyList = document.querySelector(".companyList");
+const list2 = document.querySelector(".list2");
+const FeaturesList = document.querySelector(".FeaturesList");
+const CompanyList = document.querySelector(".CompanyList");
+const itemList1 = document.querySelector(".itemList1");
+const itemList2 = document.querySelector(".itemList2");
+
+featuresList.addEventListener("click", () => {
+  list1.classList.toggle("visible");
+  list1.classList.contains("visible")
+    ? (featuresList.querySelector("img").src = "./images/icon-arrow-up.svg")
+    : (featuresList.querySelector("img").src = "./images/icon-arrow-down.svg");
+});
+
+companyList.addEventListener("click", () => {
+  list2.classList.toggle("visible");
+  list2.classList.contains("visible")
+    ? (companyList.querySelector("img").src = "./images/icon-arrow-up.svg")
+    : (companyList.querySelector("img").src = "./images/icon-arrow-down.svg");
+});
+
+FeaturesList.addEventListener("click", () => {
+  itemList1.classList.toggle("display");
+  itemList1.classList.contains("display")
+    ? (FeaturesList.querySelector("img").src = "./images/icon-arrow-up.svg")
+    : (FeaturesList.querySelector("img").src = "./images/icon-arrow-down.svg");
+});
+CompanyList.addEventListener("click", () => {
+  itemList2.classList.toggle("display");
+  itemList2.classList.contains("display")
+    ? (CompanyList.querySelector("img").src = "./images/icon-arrow-up.svg")
+    : (CompanyList.querySelector("img").src = "./images/icon-arrow-down.svg");
+});
